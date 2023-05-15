e7ee7835cbf12e71007ca303ec4804400225c544
TheWanderingCrow <torluck.kculrot@gmail.com>
2023-05-14 23:19:43 -0400

    Pushing structure and waiting until class starts to work on assignment

--- src/Loader.php
+++ src/Loader.php
@@ -3,13 +3,16 @@
 namespace CrowCMS;
 
 class Loader {
-    private String $page;
+    private $page;
     public function __construct() {
         $this->page = null;
     }
 
     public function load(String $pageName) {
         $path = __DIR__."/pages/".$pageName;
+        if (!file_exists($path)) {
+            throw new Exception("File not Found");
+        }
         $fp = fopen($path, 'r');
         $this->page = fread($fp, filesize($path));
     }
