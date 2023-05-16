560ee38a02fb5ce8d3522d2aae2a8ad11d266875
TheWanderingCrow <torluck.kculrot@gmail.com>
2023-05-16 01:30:21 -0400

    Switching to remote dev

--- src/pages/index.php
+++ src/pages/index.php
@@ -2,12 +2,12 @@
 include __DIR__ . "/../../vendor/autoload.php";
 use CrowCMS\Design;
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-    <link rel="stylesheet" href="/css/design.css" />
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <link rel="stylesheet" href="/css/design.css" type="text/css" media="all">
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
     <title>
         CSIS410 - CrowCMS
     </title>
