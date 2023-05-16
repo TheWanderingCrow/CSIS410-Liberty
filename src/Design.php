610bdc8fc7bdb3dfd736a3ac0f4fd994a7e42948
Ubuntu <ubuntu@ip-172-31-9-151.ec2.internal>
2023-05-16 16:47:28 +0000

    Added validation stickers, centered footer-text in css

--- src/Design.php
+++ src/Design.php
@@ -20,6 +20,19 @@ class Design {
         $footer = <<<HEREDOC
         <div>
             <div class="footer">
+                <div class="validation-icons">
+                    <p>
+                        <a href="http://validator.w3.org/check?uri=referer"><img
+                            src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+                        </a>
+
+                        <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                            <img style="border:0;width:88px;height:31px"
+                                src="http://jigsaw.w3.org/css-validator/images/vcss"
+                                alt="Valid CSS!" />
+                        </a>
+                    </p>
+                </div>
                 <div class="footer-text">Copyright Patrick Menking 2023</div>
                 <div class="last-edited"> Last Modified: {$dateLastModified}</div>
             </div>
