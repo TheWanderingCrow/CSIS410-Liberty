6c461f6c9114e966a75c08a6c49f678c0315af28
Ubuntu <ubuntu@ip-172-31-9-151.ec2.internal>
2023-05-17 00:43:26 +0000

    Foundations and about page

--- src/pages/foundations.php
+++ src/pages/foundations.php
@@ -1,13 +1,18 @@
 <?php
-include __DIR__ . "/../../vendor/autoload.php";
-use CrowCMS\Design;
+include __DIR__."/../../vendor/autoload.php";
+use CrowCMS;
 Design::prelude();
 ?>
 
 <body>
-    <?php Design::header();?>
-
-
-    <?php Design::footer(__FILE__);?>
+    <?php Design::header(); ?>
+    <div class="body-text">
+        <h1>Main Menu</h1>
+        <p><a href="/index.php?p=foundations/about.php">About Us</a></p>
+        <p><a href="/index.php?p=foundations/info.php">phpinfo();</a></p>
+        <p><a href="/index.php?p=foundations/contact.php">Contact Us</a></p>
+    </div>
+    <?php Design::footer(__FILE__); ?>
 </body>
+
 </html>
---
+++ src/pages/foundations/about.php
@@ -0,0 +1,23 @@
+<?php
+include __DIR__."/../../../vendor/autoload.php";
+use CrowCMS;
+Design::prelude();
+?>
+
+<body>
+    <?php Design::header(); ?>
+    <div class="body-text">
+        <h1>Main Menu</h1>
+        <p><a href="/index.php?p=/foundations/about.php">About Us</a></p>
+        <p><a href="/index.php?p=variables">Module 1: Week 1 Variables</a></p>
+        <p><a href="/index.php?p=forms">Module 2: Week 2 Forms</a></p>
+        <p><a href="/index.php?p=arrays">Module 3: Week 3 Arrays</a></p>
+        <p><a href="/index.php?p=sessions">Module 4: Week 4 Sessions</a></p>
+        <p><a href="/index.php?p=cms_sessions">Module 5: Week 5 CMS Sessions</a></p>
+        <p><a href="/index.php?p=database">Module 6: Week 6 Database</a></p>
+        <p><a href="/index.php?p=cms_database">Module 8: Week 8 CMS Database</a></p>
+    </div>
+    <?php Design::footer(__FILE__); ?>
+</body>
+
+</html>
