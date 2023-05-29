9f4aefb3ed3fd31ed8ba87ae3b5353b72cdd5f9a
Ubuntu <ubuntu@ip-172-31-9-151.ec2.internal>
2023-05-29 19:42:14 +0000

    Working on radio buttons

--- src/FormClient.php
+++ src/FormClient.php
@@ -12,7 +12,42 @@ class FormClient {
         $this->client = new SQLite3(__DIR__."../forms.db");
     }
 
-    public function create_ranker($fname, $lname) {
-        
+    public function create_ranker($fullname) {
+        $this->client->exec("INSERT INTO ranker (fullname) VALUES ({$fullname});");
+        $res = $this->client->query("SELECT id FROM ranker WHERE fullname = {$name};");
+        $res = $res->fetchArray(SQLITE3_ASSOC);
+        return $res['id'];
+    }
+
+    public function getpoll($ranker, $option, $value) {
+        $this->client->exec("INSERT INTO getchoices (ranker_id, option, value) VALUES ({$ranker}, {$option}, {$value});");
+    }
+
+    public function postpoll($ranker, $option, $value) {
+        $this->client->exec("INSERT INTO postchoices (ranker_id, option, value) VALUES ({$ranker}, {$option}, {$value});");
+    }
+
+    public function fetchresults($pollname) {
+        switch ($pollname) {
+            case 'get':
+                $res = $this->client->query("SELECT option, value FROM getchoices");
+                $rows = [];
+                while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
+                    array_push($rows, $row);
+                }
+                return $rows;
+                break;
+            case 'post':
+                $res = $this->client->query("SELECT option, value FROM postchoices");
+                $rows = [];
+                while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
+                    array_push($rows, $row);
+                }
+                return $rows;
+                break;
+            default:
+                throw new \Exception("Error Processing Request", 1);
+                break;
+        }
     }
 }
--- src/pages/forms.php
+++ src/pages/forms.php
@@ -4,3 +4,48 @@ require __DIR__."/../../vendor/autoload.php";
 
 use CrowCMS\Design;
 
+Design::prelude();
+?>
+
+<body>
+    <?php Design::header(); ?>
+    <div>
+        <form action="/index.php?p=forms/results" method="get">
+        <div>
+            <span>
+                <input type="radio" name="radio1">Strongly Disagree</input>
+                <input type="radio" id="radio1"></input>
+                <input type="radio" id="radio1">Neutral</input>
+                <input type="radio" id="radio1"></input>
+                <input type="radio" id="radio1">Strongly Agree</input>
+            </span>
+        </div>
+        <div>
+            <span>
+                <input type="radio" id="1">Strongly Disagree</input>
+                <input type="radio" id="2"></input>
+                <input type="radio" id="3">Neutral</input>
+                <input type="radio" id="4"></input>
+                <input type="radio" id="5">Strongly Agree</input>
+            </span>
+        </div>
+        <div>
+            <span>
+                <input type="radio" id="1">Strongly Disagree</input>
+                <input type="radio" id="2"></input>
+                <input type="radio" id="3">Neutral</input>
+                <input type="radio" id="4"></input>
+                <input type="radio" id="5">Strongly Agree</input>
+            </span>
+        </div>
+    </div>
+
+    <div>
+        <form action="/index.php?p=forms/results" method="post">
+        
+        </form>
+    </div>
+    <?php Design::footer(__FILE__); ?>
+</body>
+
+</html>
